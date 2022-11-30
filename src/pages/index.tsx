0045863567fef6a668f04e5742de16cdace637d4
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2022-11-30 11:13:25 -0400

    Estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,24 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  border: 'none',
+  borderRadius: 4,
+  backgroundColor: '$green500',
+  padding: '4px 8px',
+  fontWeight: 'bold',
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  }
+})
+
 export default function Home() {
   return (
-    <h1>Salve Ignite</h1>
+    <>
+      <h1>Salve Ignite</h1>
+      <Button>
+        Botão personalizado
+      </Button>
+    </>
   )
 }
