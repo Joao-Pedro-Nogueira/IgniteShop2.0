b2d1ae15f45db70db69e54b8a3f4713a31f5eae9
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2022-12-01 19:47:16 -0400

    Página Home: carrosel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,63 @@
-import { styled } from "../styles"
+import Image from "next/image"
 
-const Button = styled('button', {
-  border: 'none',
-  borderRadius: 4,
-  backgroundColor: '$green500',
-  padding: '4px 8px',
-  fontWeight: 'bold',
+import { useKeenSlider } from 'keen-slider/react'
 
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
+import { HomeContainer, Product } from "../styles/pages/home"
+
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
+import camiseta4 from '../assets/camisetas/4.png'
+
+import 'keen-slider/keen-slider.min.css'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
     <>
-      <h1>Salve Ignite</h1>
-      <Button>
-        Botão personalizado
-      </Button>
+      <HomeContainer ref={sliderRef} className='keen-slider'>
+        <Product  className="keen-slider__slide">
+          <Image src={camiseta1} width={520} height={480} alt='' priority={true} />
+
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+
+        <Product  className="keen-slider__slide">
+          <Image src={camiseta2} width={520} height={480} alt='' priority={true} />
+
+          <footer>
+            <strong>Camiseta Y</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+
+        <Product  className="keen-slider__slide">
+          <Image src={camiseta3} width={520} height={480} alt='' priority={true} />
+
+          <footer>
+            <strong>Camiseta Z</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+
+        <Product  className="keen-slider__slide">
+          <Image src={camiseta4} width={520} height={480} alt='' priority={true} />
+
+          <footer>
+            <strong>Camiseta ZX</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+      </HomeContainer>
     </>
   )
 }
