b0d0ade140dfca265c84cadfccebe590a6c4e9db
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2022-12-06 09:12:25 -0400

    Rota de checkout no Stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -12,10 +12,15 @@ interface ProductProps {
     imageUrl: string,
     price: string,
     description: string,
+    defaultPriceId: string,
   }
 }
 
 export default function Product({ product }: ProductProps) {
+
+  function handleBuyProduct() {
+    console.log(product.defaultPriceId)
+  }
   return (
     <ProductContainer>
       <ImageContainer>
@@ -28,7 +33,7 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button>
+        <button onClick={handleBuyProduct}>
           Comprar agora
         </button>
       </ProductDetails>
@@ -68,6 +73,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           currency: 'BRL',
         }).format(price.unit_amount! / 100),
         description: product.description,
+        defaultPriceId: price.id,
       }
     },
     revalidate: 60 * 60 * 1, // 1 hour in cache
